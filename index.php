15f063a5748b1b8a647f90e1cf8847be1d26f633
FazalRasel <fazalrasel@gmail.com>
2019-08-07 23:59:20 +0600

    admin: bugfix, revert back to relative library location.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $site = 'ntw';
 define("ROOT", __DIR__ . "/");
-set_include_path(ROOT . './../library');
+set_include_path(ROOT . './library');
 
 $page_id = (!empty($_GET['id'])) ? $_GET['id'] : 'main_index';
 
