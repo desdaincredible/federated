e36ffcba7ef2b43feb9b9a53806fa75af4476abf
FazalRasel <fazalrasel@gmail.com>
2019-08-11 15:31:56 +0600

    admin421: download csv file renamed with date and time.

--- library/download_csv_labor.php
+++ library/download_csv_labor.php
@@ -9,13 +9,14 @@ if (isset($_SESSION['admin_id'])) {
     $sql = "Select * from labor_claims";
     $db->query($sql);
     $claims = $db->resultset();
+    $date = date('Y_m_d_H_i_s');
     ob_clean();
     header('Pragma: public');
     header('Expires: 0');
     header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
     header('Cache-Control: private', false);
     header('Content-Type: text/csv');
-    header('Content-Disposition: attachment;filename=tire_claims.csv');
+    header("Content-Disposition: attachment;filename=labor_claims_$date.csv");
     if(isset($claims['0'])){
         $fp = fopen('php://output', 'w');
         fputcsv($fp, array_keys($claims['0']));
